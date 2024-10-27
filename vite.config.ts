e9d5769e5c26bba631cc8208d599832ee9b9159a
Enderlorde <ender.workpost@ya.ru>
2024-10-27 23:16:39 +0300

    added build script

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import postcssNested from "postcss-nested";
 export default defineConfig({
   root: "./src",
   build: {
-    outDir: "./build",
+    outDir: "../build",
     emptyOutDir: true,
   },
   plugins: [svgr({})],
